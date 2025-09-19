d28fb4e2251ac8400b02263b13dd4a21a511ab4c
Daniel Huang <daniel1.huang@intel.com>
2025-09-18 17:27:16 -0700

    Smaller batch size for better training results
    Signed-off-by: Daniel Huang <daniel1.huang@intel.com>

--- fine_tune_all.sh
+++ fine_tune_all.sh
@@ -16,9 +16,9 @@ run() {
 		--bf16 True \
 		--log_on_each_node False \
 		--num_train_epochs 1 \
-		--per_device_train_batch_size 1 \
+		--per_device_train_batch_size 2 \
 		--per_device_eval_batch_size 4 \
-		--gradient_accumulation_steps 8 \
+		--gradient_accumulation_steps 1 \
 		--ddp_find_unused_parameters False \
 		--save_strategy "no" \
 		--learning_rate 2e-5 \
